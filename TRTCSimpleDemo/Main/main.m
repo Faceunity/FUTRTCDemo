07ae9971b2d4c581731e6ee92b345dc74f96a28d
sunmu <musun@faceunity.com>
2021-01-19 09:38:07 +0800

    update nama 7.3.0

---
+++ TRTCSimpleDemo/Main/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TRTCSimpleDemo
+//
+//  Created by support on 2020/9/29.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
